ddc457e933cf483ce096c6e155a677232fd9bcb8
nugugae <89291329+nugugae@users.noreply.github.com>
2023-07-31 20:45:59 +0900

    Create DROP.sql

---
+++ DROP.sql
@@ -0,0 +1,8 @@
+drop table subject_jokbo;
+drop table answer;
+drop table question;
+drop table human_type;
+drop table school;
+drop table subjectAll;
+drop table order_info;
+drop table orderitem_info;
